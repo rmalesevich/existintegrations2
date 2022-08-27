53acfd5036d3e04456b01f2a421df18473613a6a
rmalesevich <ryan@malesevich.me>
2022-08-27 15:45:47 -0500

    Craete Onboarding page to connect to Exist

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,15 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('home');
+        $user = auth()->user();
+
+        if (!empty($user->existUser->id)) {
+            // Configure and display the home page for a user already connected to Exist
+            return view('home');
+        } else {
+            // User is not already connected to Exist
+            return view('onboard');
+        }
+        
     }
 }
