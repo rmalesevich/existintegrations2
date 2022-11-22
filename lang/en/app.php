9b90883ea76875a03577c934c91a173d73a28f6d
rmalesevich <ryan@malesevich.me>
2022-11-22 06:42:02 -0600

    Add language file support for text

---
+++ lang/en/app.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'backToHome' => 'Back to Home',
+];
--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -32,6 +32,12 @@
                     </div>
                 @endif
 
+                <div class="float-right">
+                    <a href="{{ route('home') }}" class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+                        {{ __('app.backToHome') }}
+                    </a>
+                </div>
+
                 @php
                     $availableIntegrations = 0
                 @endphp
