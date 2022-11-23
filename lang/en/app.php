2b1ecc2a9d489b0f46d5a5d0da3512b5aa6e4bff
rmalesevich <ryan@malesevich.me>
2022-11-23 07:18:58 -0600

    Move more of the WhatPulse text into the language file

--- lang/en/app.php
+++ lang/en/app.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
+    // HEADERS
     'backToHome' => 'Back to Home',
+    'connectedAs' => 'Connected as :username',
+
+    // MULTI-USE TEXT
+    'zeroOutButton' => 'Zero Out :service Data on Exist',
+    'zeroOutText' => 'If you are experiencing issues with the data that has been sent to Exist for :service, you can initiate a Zero Out of the data for the last ' . config('services.baseDays') . ' days. On the next processing of :service data, it will first zero out the records in Exist and replace them with the latest information from :service.',
+
 ];
