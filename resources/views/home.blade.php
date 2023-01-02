f0fd127f9289481a3833ac1a99176dc9248ec8e0
rmalesevich <ryan@malesevich.me>
2023-01-02 10:43:23 -0600

    Add support for global messages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                @if ($messageSet)
+                    <div class="bg-purple-100 rounded-lg py-5 px-6 mb-4 text-base text-purple-700 mb-3" role="alert">
+                        {{ $message }}
+                    </div>
+                @endif
                     
                 @if (session()->has('errorMessage'))
                     <div class="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3">
